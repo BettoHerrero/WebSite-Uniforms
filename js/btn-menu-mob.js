96074ea9fe121f5e10abbcc515aba3c6326ba6a5
Roberto de Albuquerque <146784257+BettoHerrero@users.noreply.github.com>
2024-08-28 12:39:39 -0300

    primeiro commit

---
+++ js/btn-menu-mob.js
@@ -0,0 +1,13 @@
+let btnMenuMob = document.querySelector('#btn-menu-mob')
+let line1 = document.querySelector('.line-menumob-1')
+let line2 = document.querySelector('.line-menumob-2')
+let menuMobile = document.querySelector('#menu-mobile')
+
+let body = document.querySelector('body')
+
+btnMenuMob.addEventListener("click", ()=>{
+    line1.classList.toggle('ativo1')
+    line2.classList.toggle('ativo2')
+    menuMobile.classList.toggle('abrir')
+    body.classList.toggle('no-overflow')
+})
